72ab6fd554a080487467bf9fb05afe24ab090d56
Yuti Thakkar <thakkaryuti@gmail.com>
2023-04-29 16:00:58 -0400

    adds modifying

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const YOUR_NAME = "Yuti_Thakkar";
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = `${YOUR_NAME} is the champion`;
+document.body.appendChild(newHeader);
